25df8309188b70e0d7eb380a4ae98eecd0769fbd
陆敏 <6000099@qq.com>
2019-06-13 18:10:49 +0800

    C6

---
+++ C Primer Plus(第6版)中文版/C Primer Plus(第6版)中文版/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C Primer Plus(第6版)中文版
+//
+//  Created by 陆敏 on 2019/6/13.
+//  Copyright © 2019 陆敏. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
